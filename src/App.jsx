b2d5b81e4c6304ce27764a8f51c586b772481d7f
Aryan Bhashkar <aryan056400@gmail.com>
2024-05-31 21:45:44 +0530

    31/05/2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Home from './pages/Home/Home.jsx'
 
 const App = () => {
   return (
     <div>
-      <h1>hello bitch</h1>
+      <Home/>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import './Home.css'
+import NavBar from '../../components/Navbar/Navbar.jsx'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../components/TitleCards/TitleCards.jsx'
+
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='home'>
+      <NavBar/>
+      <div className="hero">
+        <img src={hero_banner} alt='' className='banner-img'/>
+        <div className="hero-caption">
+          <img src={hero_title} alt='' className='caption-img'/>
+          <p>Discoverign the new challanges and the discoveries ,
+            of the mountains and facing the new adversities with the each new step</p>
+            <div className="hero-btns">
+              <button className='btn '><img src={play_icon} alt=''/>Play</button>
+              <button className='btn dark-btn'><img src={info_icon} alt=''/>More Info</button>
+            </div>
+            <TitleCards/>
+        </div>
+      </div>
     </div>
   )
 }
